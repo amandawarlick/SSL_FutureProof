cb0e7173bdd8856a8f00adf3a23eaff00e2e4ca0
Amanda Warlick <awarlick@uw.edu>
2024-07-05 11:01:42 -0700

    presentation figures

--- scripts/PlotTheme.R
+++ scripts/PlotTheme.R
@@ -29,11 +29,11 @@ pres_theme <- function(...) {
     axis.line.y = element_line(colour = "black"), 
     axis.line.x = element_line(colour = "black"), 
     plot.background = element_rect(), 
-    panel.background = element_rect(fill = 'white'),
-    panel.border = element_rect(fill = NA),
+    panel.background = element_rect(fill = 'white'), 
+    #panel.border = element_rect(fill = NA), #for square around plot
     panel.grid = element_blank(), 
     legend.key = element_blank(),
-    strip.background = element_blank(), 
+    strip.background = element_rect(fill = "white", color = "black"), 
     strip.text = element_text(size = 11),
     legend.text = element_text(size = 11),
     ...)
